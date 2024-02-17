bb63fd188cf163d88e4478bd77aee9ccc222f473
Mustafa Shujaie <mfshujaie@gmail.com>
2024-02-17 20:13:52 +0400

    label class vs tests

---
+++ src/protocol/label.spec.ts
@@ -0,0 +1,37 @@
+import { Label } from './label';
+
+describe('Label::encode()', () => {
+  it('should correctly encode label string to an array of octets', () => {
+    const labelStr = 'www';
+    const buffer = Label.encode(labelStr);
+
+    expect(buffer[0]).toBe(3);
+    expect(buffer[1]).toBe(0x77);
+    expect(buffer[2]).toBe(0x77);
+    expect(buffer[3]).toBe(0x77);
+    expect(buffer.length).toBe(4);
+  });
+});
+
+describe('Label::decode()', () => {
+  it('should correctly decode a label buffer to its string representation', () => {
+    const www = new Uint8Array([3, 0x77, 0x77, 0x77]);
+    const wwwStr = Label.decode(www);
+
+    expect(wwwStr).toBe('www');
+
+    const maxLengthLabel = Buffer.alloc(64, 0x77, 'ascii');
+    maxLengthLabel.writeUInt8(63);
+    const maxLengthLabelStr = Label.decode(maxLengthLabel);
+
+    expect(maxLengthLabelStr.length).toBe(63);
+    expect(maxLengthLabelStr[0]).toBe('w');
+    expect(maxLengthLabelStr[62]).toBe('w');
+  });
+
+  it("should throw if the length octet does not match label's length", () => {
+    const invalidLengthLabel = Buffer.alloc(64, 0x77, 'ascii');
+    invalidLengthLabel.writeUInt8(5);
+    expect(() => Label.decode(invalidLengthLabel)).toThrow();
+  });
+});
---
+++ src/protocol/label.ts
@@ -0,0 +1,34 @@
+import { DataFormatError } from '../error/data-error';
+
+export class Label extends Buffer {
+  static encode(label: string): Label {
+    const { length } = label;
+    if (length > 63) {
+      throw new DataFormatError(`Invalid label length: ${label} (${length})`);
+    }
+
+    const buffer = Buffer.alloc(length + 1, 0, 'ascii');
+
+    buffer.writeUint8(length, 0);
+    buffer.set(Buffer.from(label, 'ascii'), 1);
+
+    return Label.from(buffer);
+  }
+
+  static decode(array: Uint8Array): string {
+    const length = array[0];
+    const label = array.slice(1);
+
+    if (label.length !== length) {
+      throw new Error(
+        `Invalid label array input: length octet does not match label's length`
+      );
+    }
+
+    return new TextDecoder().decode(label);
+  }
+
+  toString() {
+    return this.subarray(1).toString();
+  }
+}
