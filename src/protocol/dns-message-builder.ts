d5e3ba38ec078578a4bc59a643af9e72b9c7a4db
Mustafa Shujaie <mfshujaie@gmail.com>
2024-03-03 19:10:19 +0400

    completes DNSMessageBuilder class

--- src/protocol/dns-message-builder.ts
+++ src/protocol/dns-message-builder.ts
@@ -1,10 +1,14 @@
 import { DNSMessage } from './dns-message';
 import { DNSMessageHeader } from './header';
 import { QuestionEntry } from './question';
+import { ResourceRecord } from './resource-record';
 
 export class DNSMessageBuilder {
   private header: DNSMessageHeader;
   private questions: QuestionEntry[] = [];
+  private answer: ResourceRecord[] = [];
+  private authority: ResourceRecord[] = [];
+  private additional: ResourceRecord[] = [];
 
   constructor() {
     this.header = new DNSMessageHeader({ id: 1 });
@@ -20,7 +24,48 @@ export class DNSMessageBuilder {
     return this;
   }
 
+  addQuestion(question: QuestionEntry): DNSMessageBuilder {
+    this.questions.push(question);
+    return this;
+  }
+
+  withAnswer(resourceRecords: ResourceRecord[]): DNSMessageBuilder {
+    this.answer = resourceRecords;
+    return this;
+  }
+
+  addAnswer(resourceRecord: ResourceRecord): DNSMessageBuilder {
+    this.answer.push(resourceRecord);
+    return this;
+  }
+
+  withAuthority(resourceRecords: ResourceRecord[]): DNSMessageBuilder {
+    this.authority = resourceRecords;
+    return this;
+  }
+
+  addAuthority(resourceRecord: ResourceRecord): DNSMessageBuilder {
+    this.authority.push(resourceRecord);
+    return this;
+  }
+
+  withAdditional(resourceRecords: ResourceRecord[]): DNSMessageBuilder {
+    this.additional = resourceRecords;
+    return this;
+  }
+
+  addAdditional(resourceRecord: ResourceRecord): DNSMessageBuilder {
+    this.additional.push(resourceRecord);
+    return this;
+  }
+
   build(): DNSMessage {
-    return new DNSMessage(this.header, this.questions);
+    return new DNSMessage(
+      this.header,
+      this.questions,
+      this.answer,
+      this.authority,
+      this.additional
+    );
   }
 }
--- src/protocol/dns-message.ts
+++ src/protocol/dns-message.ts
@@ -1,9 +1,13 @@
 import { DNSMessageHeader } from './header';
 import { QuestionEntry } from './question';
+import { ResourceRecord } from './resource-record';
 
 export class DNSMessage {
   constructor(
     public header: DNSMessageHeader,
-    public questions: QuestionEntry[]
+    public questions: QuestionEntry[],
+    public answer: ResourceRecord[],
+    public authority: ResourceRecord[],
+    public additional: ResourceRecord[]
   ) {}
 }
