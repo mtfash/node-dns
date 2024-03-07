a1765bfd9f3f7481ee2595e443d9767adac8e317
Mustafa Shujaie <mfshujaie@gmail.com>
2024-03-07 21:29:24 +0400

    test cname, aaaa, and multiple RRs in a message

--- src/protocol/dns-encoder.spec.ts
+++ src/protocol/dns-encoder.spec.ts
@@ -4,9 +4,9 @@ import { QCLASS } from '../values/qclass';
 import { DNSEncoder } from './dns-encoder';
 import { DNSMessage } from './dns-message';
 import { QuestionEntry } from './question';
+import { ResourceRecord } from './resource-record';
 import { DNSMessageHeader } from './header';
 import { DNSMessageBuilder } from './dns-message-builder';
-import { ResourceRecord } from './resource-record';
 
 const builder = new DNSMessageBuilder();
 const entries: { message: DNSMessage; base64: string }[] = [
@@ -88,6 +88,88 @@ const entries: { message: DNSMessage; base64: string }[] = [
       .build(),
     base64:
       'IweBgAABAAIAAAAACWNvbGxlY3RvcgZnaXRodWIDY29tAAABAAHADAAFAAEAAAKJABUSZ2xiLWRiNTJjMmNmOGJlNTQ0wBbAMgABAAEAAAAyAASMUnEV',
+  },
+  {
+    message: builder
+      .withHeader(
+        new DNSMessageHeader({
+          id: 0x6be9,
+          recursionDesired: true,
+          isQuery: true,
+          qdcount: 1,
+        })
+      )
+      .withQuestions([
+        new QuestionEntry({
+          qname: 'mesu.apple.com',
+          qtype: QTYPE.A,
+          qclass: QCLASS.IN,
+        }),
+      ])
+      .build(),
+    base64: 'a+kBAAABAAAAAAAABG1lc3UFYXBwbGUDY29tAAABAAE=',
+  },
+  {
+    message: builder
+      .withHeader(
+        new DNSMessageHeader({
+          id: 0x620f,
+          recursionDesired: true,
+          isQuery: true,
+          qdcount: 1,
+        })
+      )
+      .withQuestions([
+        new QuestionEntry({
+          qname: 'ocsp2-lb.apple.com.akadns.net',
+          qtype: QTYPE.AAAA,
+          qclass: QCLASS.IN,
+        }),
+      ])
+      .build(),
+    base64: 'Yg8BAAABAAAAAAAACG9jc3AyLWxiBWFwcGxlA2NvbQZha2FkbnMDbmV0AAAcAAE=',
+  },
+  {
+    message: builder
+      .withHeader(
+        new DNSMessageHeader({
+          id: 0x620f,
+          isQuery: false,
+          recursionDesired: true,
+          recursionAvailable: true,
+          qdcount: 1,
+          ancount: 3,
+        })
+      )
+      .withAnswer([
+        new ResourceRecord({
+          name: 'ocsp2-lb.apple.com.akadns.net',
+          type: QTYPE.CNAME,
+          cls: CLASS.IN,
+          ttl: 12,
+          rdlength: 21,
+          rdata: 'ocsp2.g.aaplimg.com',
+        }),
+        new ResourceRecord({
+          name: 'ocsp2.g.aaplimg.com',
+          type: QTYPE.AAAA,
+          cls: CLASS.IN,
+          ttl: 6,
+          rdlength: 16,
+          rdata: '2a01:b740:a14:f000::201',
+        }),
+        new ResourceRecord({
+          name: 'ocsp2.g.aaplimg.com',
+          type: QTYPE.AAAA,
+          cls: CLASS.IN,
+          ttl: 6,
+          rdlength: 16,
+          rdata: '2a01:b740:a14:f000::197',
+        }),
+      ])
+      .build(),
+    base64:
+      'Yg+BgAABAAMAAAAACG9jc3AyLWxiBWFwcGxlA2NvbQZha2FkbnMDbmV0AAAcAAHADAAFAAEAAAAMABUFb2NzcDIBZwdhYXBsaW1nA2NvbQDAOwAcAAEAAAAGABAqAbdAChTwAAAAAAAAAAIBwDsAHAABAAAABgAQKgG3QAoU8AAAAAAAAAABlw==',
   },
 ];
 
